4b85df0627471464bd8e62a5b465bb0e1fe4568a
Joao Vitor <jvitorgotardo0514@gmail.com>
2025-05-13 11:41:41 -0300

    Adiciona os arquivos da classe Plano, atualizações na classe Aluno e no TesteHeranca, conforme requisições da atividade

--- src/modelo/Aluno.java
+++ src/modelo/Aluno.java
@@ -4,6 +4,9 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +17,9 @@ import java.util.List;
 public class Aluno extends Pessoa{
     private String matricula;
     private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+    private LocalDate datamatricula;
+    private Plano plano;
+    private double valormensalidade;
     
     public void adicionarAvaliacao(AvaliacaoFisica avaliacao) {
         avaliacoes.add(avaliacao);
@@ -35,10 +41,47 @@ public class Aluno extends Pessoa{
         this.matricula = matricula;
     }
 
+    public LocalDate getDatamatricula() {
+        return datamatricula;
+    }
+
+    public void setDatamatricula(LocalDate datamatricula) {
+        this.datamatricula = datamatricula;
+    }
+
+    public double getValormensalidade() {
+        return valormensalidade;
+    }
+
+    public void setValormensalidade(double valormensalidade) {
+        this.valormensalidade = valormensalidade;
+    }
+
+    public Plano getPlano() {
+        return plano;
+    }
+
+    public void setPlano(Plano plano) {
+        this.plano = plano;
+        setValormensalidade(plano.getValor());
+        verificaDesconto(getDatamatricula());
+    }
+    
+    public double verificaDesconto(LocalDate data_Matricula){
+        Period p = Period.between(datamatricula, LocalDate.now());
+        if(p.getMonths() >= 3){
+            setValormensalidade(getValormensalidade() - (getValormensalidade() * 0.1));
+        }else{
+            setValormensalidade(getValormensalidade());
+        }
+        return getValormensalidade();
+    }
+
     @Override
     public String exibirDados() {
         String aux = super.exibirDados();
         aux += "\nMatricula: "+matricula;
+        aux += "\nValor da Mensalidade: R$"+valormensalidade;
         aux += "\nAvaliações Físicas Realizadas: " 
                 + avaliacoes.size() + "\n";
         return aux;
--- src/teste/TesteHeranca.java
+++ src/teste/TesteHeranca.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package teste;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import modelo.Aluno;
 import modelo.AvaliacaoFisica;
+import modelo.Plano;
 import modelo.Professor;
 
-
-/**
- *
- * @author vanessalagomachado
- */
 public class TesteHeranca {
     public static void main(String[] args) {
         Professor p = new Professor();
@@ -22,26 +14,44 @@ public class TesteHeranca {
         p.setCpf("123.123.123-67");
         p.setEspecializacao("Musculação");
         
-        System.out.println(p.exibirDados());
+        //System.out.println(p.exibirDados());
+        Plano plano = new Plano("Básico", "Direito a 4 horas por semana de academia", 295.00);
         
-        Aluno a = new Aluno();
-        a.setNome("Vivente");
-        a.setMatricula("123");
+        ///Aluno com direito ao desconto
+        Aluno a1 = new Aluno();
+        a1.setNome("Vivente");
+        a1.setMatricula("123");
         DateTimeFormatter formato = 
                 DateTimeFormatter.ofPattern("dd/MM/yyyy");
         
-        a.setDataNascimento(LocalDate.parse("10/01/2008", 
+        a1.setDataNascimento(LocalDate.parse("10/01/2008", 
                 formato));
+        a1.setDatamatricula(LocalDate.parse("25/11/2024", 
+                formato));
+        a1.setPlano(plano);
         
-        
-        AvaliacaoFisica av1 = new AvaliacaoFisica(a);
-        a.adicionarAvaliacao(av1);
+        AvaliacaoFisica av1 = new AvaliacaoFisica(a1);
+        a1.adicionarAvaliacao(av1);
         av1.setProfessor(p);
         
-        System.out.println(a.exibirDados());
+        System.out.println(a1.exibirDados());
+        System.out.println("--------------");
         
-        System.out.println("---- ");
-        System.out.println(av1.exibirDados());
+        //Aluno sem direito ao desconto
+        Aluno a2 = new Aluno();
+        a2.setNome("Indivíduo");
+        a2.setMatricula("254");
         
+        a2.setDataNascimento(LocalDate.parse("25/06/2003",
+                formato));
+        a2.setDatamatricula(LocalDate.parse("11/05/2025", 
+                formato));
+        a2.setPlano(plano);
+        
+        AvaliacaoFisica av2 = new AvaliacaoFisica(a2);
+        a2.adicionarAvaliacao(av2);
+        av2.setProfessor(p);
+        
+        System.out.println(a2.exibirDados());
     }
 }
